47b059e35bf7df40d982f3534cac682f90b26044
Barbie <barbie@missbarbell.co.uk>
2014-05-05 07:07:28 +0000

    Import of BARBIE/Labyrinth-DIUtils-ImageMagick-5.06 from CPAN.
    gitpan-cpan-distribution: Labyrinth-DIUtils-ImageMagick
    gitpan-cpan-version:      5.06
    gitpan-cpan-path:         BARBIE/Labyrinth-DIUtils-ImageMagick-5.06.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Labyrinth/DIUtils/ImageMagick.pm
+++ lib/Labyrinth/DIUtils/ImageMagick.pm
@@ -3,7 +3,7 @@ package Labyrinth::DIUtils::ImageMagick;
 use warnings;
 use strict;
 
-our $VERSION = '5.05';
+our $VERSION = '5.06';
 
 =head1 NAME
 
@@ -168,7 +168,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2002-2013 Barbie for Miss Barbell Productions
+  Copyright (C) 2002-2014 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
